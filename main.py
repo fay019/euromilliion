6c54bee6c2e92e3e173207568888c86a3fd38617
faycal moussouni <fay019@gmail.com>
2022-07-23 04:24:29 +0200

    random list is returned in string
    add asking for leave or make new random

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from my_random import random_numbers
+from my_random import random_numbers, exit_or_continue
 
-print(f"Euro Million random \nnumber: {random_numbers(5, 50)} \nstars : {random_numbers(2, 12)}  ")
+first_time = True
+
+while True:
+    if first_time:
+        print(f"Euro Million random \nnumber: {random_numbers(5, 50)} \nstars : {random_numbers(2, 12)}  ")
+        first_time = False
+    choice = exit_or_continue()
+    if choice.lower() == "n":
+        break
+    elif choice.lower() == "y":
+        first_time = True
+    elif choice.lower() != "y":
+        print("Only \"Y\" or \"N\" are valide")
--- my_random.py
+++ my_random.py
@@ -4,4 +4,9 @@ import random
 def random_numbers(size, avg):
     liste = random.sample(range(1, avg + 1), size)
     liste.sort()
-    return liste
+    return ', '.join(str(a) for a in liste)
+
+
+def exit_or_continue():
+    key = input("Press \"Y\" to get new random number or \"N\" to exit: ")
+    return key
